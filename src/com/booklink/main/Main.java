d5b4c0243cd44ea9b3ceed8b4fee712467cb2748
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-01-16 17:12:35 +0200

    Update Main to demonstrate Proxy pattern

--- src/com/booklink/main/Main.java
+++ src/com/booklink/main/Main.java
@@ -1,13 +1,19 @@
 package com.booklink.main;
+import com.booklink.models.Book;
 import com.booklink.models.User;
 import com.booklink.patterns.factory.AdminUserFactory;
 import com.booklink.patterns.factory.GuestUserFactory;
 import com.booklink.patterns.factory.MemberUserFactory;
 import com.booklink.patterns.factory.UserFactory;
+import com.booklink.patterns.proxy.BookService;
+import com.booklink.patterns.proxy.BookServiceImp;
+import com.booklink.patterns.proxy.BookServiceProxy;
 import com.booklink.patterns.singleton.DatabaseConnection;
 
 public class Main {
     public static void main(String[] args) {
+
+        // 1. Test Singleton
         DatabaseConnection db1 = DatabaseConnection.getInstance();
         DatabaseConnection db2 = DatabaseConnection.getInstance();
 
@@ -17,7 +23,7 @@ public class Main {
         db1.executeQuery("SELECT * FROM books");
         db2.executeQuery("SELECT * FROM authors");
 
-        // Create a Guest user
+        // 2. Test Factory + Builder: Create different users
         UserFactory guestFactory = new GuestUserFactory();
         User guest = guestFactory.createUser(
                 "Mohammad",
@@ -52,5 +58,41 @@ public class Main {
                 "Male"
         );
         admin.showPermissions();
+
+        // 3. Test Proxy: Role-based access to BookService
+        //    a) Create the real service
+        BookService realService = new BookServiceImp();
+
+        //    b) Wrap the service with a proxy for the guest user
+        BookService guestProxy = new BookServiceProxy(realService, guest);
+
+        //    c) Try some operations
+        System.out.println("\n--- Guest tries to add a book ---");
+        guestProxy.addBook(new Book(2, "Effective Java", "Joshua Bloch", "Addison-Wesley", "Science", "978-0134685991"));
+
+        System.out.println("\n--- Guest tries to borrow a book ---");
+        guestProxy.borrowBook(2);
+
+        //    d) Wrap the service with a proxy for the member user
+        BookService memberProxy = new BookServiceProxy(realService, member);
+
+        System.out.println("\n--- Member tries to add a book ---");
+        memberProxy.addBook(new Book(3, "Java Concurrency in Practice", "Brian Goetz", "Addison-Wesley", "Science", "978-0321349606"));
+
+        System.out.println("\n--- Member borrows a book ---");
+        memberProxy.borrowBook(3);
+
+        //    e) Wrap the service with a proxy for the admin user
+        BookService adminProxy = new BookServiceProxy(realService, admin);
+
+        System.out.println("\n--- Admin tries to add a book ---");
+        adminProxy.addBook(new Book(4, "Head First Design Patterns", "Eric Freeman", "O'Reilly Media", "Science", "978-0596007126"));
+
+        System.out.println("\n--- Admin borrows a book ---");
+        adminProxy.borrowBook(4);
+
+        System.out.println("\n--- Admin removes a book ---");
+        adminProxy.removeBook(2);
     }
+
 }
