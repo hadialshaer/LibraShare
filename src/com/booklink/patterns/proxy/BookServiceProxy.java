2cec2fd253dbb877e8ebe38ca70b9b703afe7621
hadialshaerr <162624238+hadialshaerr@users.noreply.github.com>
2025-01-16 23:01:18 +0200

    Update BookServiceProxy to trigger notifications when books are returned

--- src/com/booklink/patterns/proxy/BookServiceProxy.java
+++ src/com/booklink/patterns/proxy/BookServiceProxy.java
@@ -3,11 +3,13 @@ package com.booklink.patterns.proxy;
 import com.booklink.models.Book;
 import com.booklink.models.User;
 import com.booklink.models.UserRole;
+import com.booklink.patterns.observer.BookAvailabilityNotifier;
 
 public class BookServiceProxy implements BookService {
 
     private final BookService realService;
     private final User currentUser;
+    private static final BookAvailabilityNotifier notifier = new BookAvailabilityNotifier();
 
     public BookServiceProxy(BookService realService, User currentUSer) {
         this.realService = realService;
@@ -35,6 +37,7 @@ public class BookServiceProxy implements BookService {
     public void borrowBook(int bookId) {
         if(currentUser.getRole().equals(UserRole.ADMIN) || currentUser.getRole().equals(UserRole.MEMBER)){
             realService.borrowBook(bookId);
+            System.out.println(currentUser.getFirstName() + " borrowed book: " + bookId);
         }
         else System.out.println("Access Denied: Only members or admin can borrow books.");
     }
@@ -43,8 +46,18 @@ public class BookServiceProxy implements BookService {
     public void returnBook(int bookId) {
         if(currentUser.getRole().equals(UserRole.ADMIN) || currentUser.getRole().equals(UserRole.MEMBER)){
             realService.returnBook(bookId);
+            System.out.println(currentUser.getFirstName() + " returned book: " + bookId);
+            notifier.notifyUsers(bookId); // Notify waiting users
         }
         else System.out.println("Access Denied: Only members or admin can return books.");
 
+    }
+
+    public void subscribeToBook(int bookId) {
+        if (currentUser.getRole().equals(UserRole.MEMBER) || currentUser.getRole().equals(UserRole.ADMIN)) {
+            notifier.subscribe(bookId, currentUser);
+        } else {
+            System.out.println("Access Denied: Guests cannot subscribe for book notifications");
+        }
     }
 }
